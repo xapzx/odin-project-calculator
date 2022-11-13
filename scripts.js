1e6112a286c9022d95e29765fba37ad11279004d
Andy Phan <a1785560@student.adelaide.edu.au>
2022-11-13 18:29:19 +1030

    Created a new function operate that takes an operator and 2 numbers and then calls one of the maths functions on the numbers

--- scripts.js
+++ scripts.js
@@ -13,3 +13,15 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
     return number1 / number2;
 }
+
+function operate(operator, number1, number2) {
+    if(operator === "+") {
+        return add(number1, number2);
+    } else if(operator === "-") {
+        return subtract(number1, number2);
+    } else if(operator === "*") {
+        return multiply(number1, number2);
+    } else if(operator === "/") {
+        return divide(number1, number2);
+    }
+}
